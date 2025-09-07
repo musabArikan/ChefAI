c8a4752c023aad908e8ededcb69a5c350fecdd75
Musab Arıkan <arikan950@hotmail.com>
2025-09-07 17:27:28 +0300

    Refactor Home component: add foodRef and update handleCategoryChange for smooth scrolling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "../components/Header";
 import ExploreMenu from "../components/ExploreMenu";
 import FoodDisplay from "../components/FoodDisplay";
@@ -6,11 +6,28 @@ import AppDownload from "../components/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
+  const foodRef = useRef(null);
+
+  const handleCategoryChange = (cat) => {
+    setCategory(cat);
+    setTimeout(() => {
+      if (foodRef.current) {
+        const navbarHeight = 200;
+        const y =
+          foodRef.current.getBoundingClientRect().top +
+          window.pageYOffset -
+          navbarHeight;
+        window.scrollTo({ top: y, behavior: "smooth" });
+      }
+    }, 100);
+  };
   return (
     <div>
       <Header />
-      <ExploreMenu category={category} setCategory={setCategory} />
-      <FoodDisplay category={category} />
+      <ExploreMenu category={category} setCategory={handleCategoryChange} />
+      <div ref={foodRef} id="food-display-scroll-point">
+        <FoodDisplay category={category} />
+      </div>
       <AppDownload />
     </div>
   );
